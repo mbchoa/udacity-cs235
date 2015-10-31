a83390a47e828ccd9deba0e784b1dcfd9818e6ad
Michael-Bryant Choa <mbchoa@gmail.com>
2015-10-31 00:18:02 -0400

    Add Jinja2 library for templating Add lesson2/templates dir for using HTML templates Learned: - how to use templates to separate out python and html - how to use template inheritance to create modular content blocks

--- main.py
+++ main.py
@@ -1,14 +1,38 @@
+import os
+
 import webapp2
+import jinja2
 
 from lesson2.cipher import Lesson2Rot13
-from lesson2.signup import Lesson2Signup
-from lesson2.signup import Lesson2SignupThanks
+from lesson2.signup import Lesson2Signup, Lesson2SignupThanks
 
-class MainPage(webapp2.RequestHandler):
+template_dir = os.path.join(os.path.dirname(__file__), 'lesson2\\templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+	def write(self, *a, **kw):
+		self.response.out.write(*a, **kw)
+
+	def render_str(self, template, **params):
+		t = jinja_env.get_template(template)
+		return t.render(params)
+
+	def render(self, template, **kw):
+		self.write(self.render_str(template, **kw))
+
+class MainPage(Handler):
 	def get(self):
-		self.redirect('/lesson2/signup')
-		
+		items = self.request.get_all("food")
+		self.render("shopping_list.html", items = items)
+
+class FizzBuzzHandler(Handler):
+	def get(self):
+		n = self.request.get('n', 0)
+		n = n and int(n)
+		self.render("fizzbuzz.html", n = n)
+
 app = webapp2.WSGIApplication([('/', MainPage),
 							   ('/lesson2/rot13', Lesson2Rot13),
 							   ('/lesson2/signup', Lesson2Signup),
-							   ('/lesson2/signup/thanks', Lesson2SignupThanks)], debug=True)
+							   ('/lesson2/signup/thanks', Lesson2SignupThanks),
+							   ('/lesson2/templates/fizzbuzz', FizzBuzzHandler)], debug=True)
